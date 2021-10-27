b2007ff81b13928bbb211ec22bcf2bce503377b3
Sergey stdKit <s.uferov@stdkit.ru>
2021-10-27 13:02:08 +0300

    developer-page; jk detail page

---
+++ source/fancybox.custom.js
@@ -0,0 +1,9 @@
+// Fancybox.defaults.closeButton = 'outside'
+Fancybox.defaults.autoFocus = false
+
+Fancybox.bind('[data-fancybox-modal]', {
+  // type: 'ajax',
+  dragToClose: false,
+  mainClass: 'fancybox-custom-modal',
+  // closeButton: 'outside'
+})
