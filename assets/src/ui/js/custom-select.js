5bf377414f8d34c00d62c733113b73f0dfe20e31
qp0kycHuk <serega990112@mail.ru>
2022-04-18 17:25:53 +0300

    some organisation moments

--- assets/src/ui/js/custom-select.js
+++ assets/src/ui/js/custom-select.js
@@ -3,7 +3,6 @@ import customSelect from '/vendors/custom-select/';
 
 const init = () => {
   customSelect('[data-custom-select]', {
-
     prefix: (select) => select.getAttribute('data-prefix'),
     prefixClasses: ['mr-2', 'text--demibold']
   });
