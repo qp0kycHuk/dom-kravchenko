3ae90a49b0a43392ad663a87cad69e67d08df402
qp0kycHuk <serega990112@mail.ru>
2022-05-04 15:10:45 +0300

    add index scss

--- assets/src/index.js
+++ assets/src/index.js
@@ -4,38 +4,24 @@ import loadFile from './js/load-file';
 import showPass from './js/show-pass';
 import theme from './js/theme';
 import UI from './ui/js/index'
-// import * as FilePond from 'filepond';
-// import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-// import filePondRU from 'filepond/locale/ru-ru.js';
-
-// import 'filepond/dist/filepond.css';
-// import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 
+import './index.scss'
 
 
 window.tippy = tippy
 window.draggable = { Sortable, Plugins }
-// window.FilePond = FilePond
 
 
 showPass.init();
 loadFile.init();
 
-// FilePond.setOptions(filePondRU);
-// FilePond.registerPlugin(FilePondPluginImagePreview);
-// FilePond.registerPlugin(
-//   FilePondPluginImagePreview,
-//   FilePondPluginImageExifOrientation,
-//   FilePondPluginFileValidateSize,
-//   FilePondPluginImageEdit
-// );
+
 
 
 UI.init()
 
 function loadHandler() {
   theme.init();
-  // FilePond.create( document.querySelector('[data-load-file]') );
 }
 window.addEventListener('DOMContentLoaded', () => loadHandler())
